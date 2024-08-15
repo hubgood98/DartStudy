bf62ac7868d2c0aef3ccdcf528db9ca6196885ae
hubgood98 <heej1998@gmail.com>
2024-08-15 21:52:35 +0900

    기초 변수

---
+++ src/variable1.kt
@@ -0,0 +1,15 @@
+
+const val num = 20 //Top레벨 상수 가장 우선적으로 호출
+
+fun main() {
+
+    var a = 10
+    var i : Int = 10  //형 추론을 기본적으로 하지만 이렇게 명시적으로 지정도 가능 (대문자로 타입지정해야함)
+    //String, Int, Double 등
+    var name = "heejune"
+    println("Hello World!" + i)
+
+    //상수는 val을 사용(불변, 재 대입 할 수x, java의 final과 동일)
+
+    val num = 20;
+}
