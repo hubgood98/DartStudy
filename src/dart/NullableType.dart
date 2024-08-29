0667a8910073c2a56bc380d91ecb717e6d618595
hubgood98 <heej1998@gmail.com>
2024-08-29 22:40:35 +0900

    임시저장

--- src/dart/NullableType.dart
+++ src/dart/NullableType.dart
@@ -3,4 +3,14 @@ void main()
   //타입 뒤에 ?를 명시하여 null값을 가질 수 있다.
   double? number1 = 1;
 
+  //?명시하지 않아 오류 발생
+  //double number2 = null;
+
+  //?를 입력했을때 기본값으로 null 저장된다
+  int? valA;
+  print(valA);
+
+  //기본값이 null일경우 3 저장
+  valA ??= 3;
+  print(valA);
 }
